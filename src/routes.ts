1886e8085b5d735e8678db9517d753d6f21a0a2e
Olistar <130336147+Olanakelbesa@users.noreply.github.com>
2026-04-27 10:32:18 +0300

    refactor(routes): remove recommendation routes from main router

--- src/routes.ts
+++ src/routes.ts
@@ -7,7 +7,6 @@ import appointmentRoutes from './modules/appointments/routes';
 import notificationRoutes from './modules/notifications/routes';
 import adminRoutes from './modules/admin/routes';
 import reviewRoutes from './modules/review-rate/routes';
-import recommendationRoutes from "./modules/recommendation/routes";
 
 const router = Router();
 
@@ -34,8 +33,5 @@ router.use('/admin', adminRoutes);
 
 //Review Routes
 router.use('/review', reviewRoutes);
-
-//RecommendationRoute
-router.use("/recommend", recommendationRoutes);
 
 export default router;
